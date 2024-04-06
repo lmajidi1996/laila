4b8b126ddae1c9eee98d38290a664fe0cbdaf86a
laila1996 <lmajidi@students.pccc.edu>
2024-04-05 21:09:33 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int[] array = {10, 5, 7, 15, 3, 20, 8};
+
+        for (int i = 0; i < array.length; i++) {
+
+        }
+        Arrays.sort(array);
+        System.out.println("array[-2] = " + array[5]);
+    }
+
+}
